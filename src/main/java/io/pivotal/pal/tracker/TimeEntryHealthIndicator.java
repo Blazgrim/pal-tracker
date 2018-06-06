01b54e2421cd58ff24d04510071b6608ef0391b5
Micah <mjkaiser@mtu.edu>
2018-06-06 09:53:44 -0600

    Actuator

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryHealthIndicator.java
@@ -0,0 +1,28 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TimeEntryHealthIndicator implements HealthIndicator {
+
+    private TimeEntryRepository timeEntryRepository;
+
+    public TimeEntryHealthIndicator(TimeEntryRepository timeEntryRepository)
+    {
+        this.timeEntryRepository = timeEntryRepository;
+    }
+
+    @Override
+    public Health health() {
+        Health.Builder healthBuilder = new Health.Builder();
+        if (timeEntryRepository.list().size() < 5) {
+            healthBuilder.up();
+        }
+        else {
+            healthBuilder.down();
+        }
+        return healthBuilder.build();
+    }
+}
